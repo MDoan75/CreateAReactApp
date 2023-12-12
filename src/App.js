33474019caeeea77971238ce45a883586e2054e0
MDoan75 <141285754+MDoan75@users.noreply.github.com>
2023-12-12 16:53:24 -0500

    Movie Finder Two

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Home from './Home';
+import Movie from './Movie';
 import './App.css';
 const NotFound = () => {
   return <h2>404 Not Found</h2>;
@@ -13,6 +14,7 @@ const App = () => {
       </nav>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/movie/:id" component={Movie} />
         <Route component={NotFound} />
       </Switch>
     </Router>
