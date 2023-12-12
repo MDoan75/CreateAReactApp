15facb5c1318728d6bd5cbed2951fed5f1456c42
MDoan75 <141285754+MDoan75@users.noreply.github.com>
2023-12-12 17:20:40 -0500

    Movie Finder Two

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { json, checkStatus } from './utils';
+
 const Movie = (props) => {
   const {
     Title,
@@ -9,6 +10,7 @@ const Movie = (props) => {
     Type,
     Poster,
   } = props.movie;
+
   return (
     <div className="row">
       <div className="col-4 col-md-2 col-lg-1 mb-3">
@@ -25,6 +27,7 @@ const Movie = (props) => {
     </div>
   )
 }
+
 class MovieFinder extends React.Component {
   constructor(props) {
     super(props);
@@ -33,12 +36,15 @@ class MovieFinder extends React.Component {
       results: [],
       error: '',
     };
+
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+
   handleChange(event) {
     this.setState({ searchTerm: event.target.value });
   }
+
   handleSubmit(event) {
     event.preventDefault();
     let { searchTerm } = this.state;
@@ -46,6 +52,7 @@ class MovieFinder extends React.Component {
     if (!searchTerm) {
       return;
     }
+
     fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=ac1c08b3`)
       .then(checkStatus)
       .then(json)
@@ -53,6 +60,7 @@ class MovieFinder extends React.Component {
         if (data.Response === 'False') {
           throw new Error(data.Error);
         }
+
         if (data.Response === 'True' && data.Search) {
           console.log(data);
           this.setState({ results: data.Search, error: '' });
@@ -63,8 +71,10 @@ class MovieFinder extends React.Component {
         console.log(error);
       })
   }
+
   render() {
     const { searchTerm, results, error } = this.state;
+
     return (
       <div className="container">
         <div className="row">
@@ -93,4 +103,5 @@ class MovieFinder extends React.Component {
     )
   }
 }
+
 export default MovieFinder;
